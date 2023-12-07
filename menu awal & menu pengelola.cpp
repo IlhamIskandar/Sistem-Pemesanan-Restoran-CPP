a73429b4618734fcc88d0a1436163a297ca1e465
IlhamIskandar <muhammadilhamiskandar1@gmail.com>
2023-12-07 21:31:02 +0700

    first commit

---
+++ menu awal & menu pengelola.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+    int main(){
+        int pilihan, admin;
+        string username="kelompok4", password="pemesananmenu", menu, inputUsername, inputPassword;
+        string namaMakanan[20];
+        int hargaMakanan[20];
+        int menuPengelola;
+
+        do{
+            cout<<"=======Pilih Pengguna=======\n";
+            cout<<"1.Pengelola"<<endl;
+            cout<<"2.Pembeli"<<endl;
+            cout<<"3.Exit"<<endl;
+            cin>>pilihan;
+
+            switch (pilihan){
+                case 1:
+                    do{
+                    cout<<"Silahkan Login Terlebih Dahulu"<<endl;
+                    cout<<"Username :";
+                    cin>>inputUsername;
+                    cout<<"Password :";
+                    cin>>inputPassword;
+
+                    if (username!=inputUsername || password!=inputPassword){
+                        cout<<"Username atau Password Salah!"<<endl;
+                        }else {
+                            system("cls");
+                            cout<<"Berhasil Login"<<endl;
+                        }
+                    }while (username!=inputUsername || password!=inputPassword);
+
+                    do{
+                        cout<<"=======Menu Pengelola========="<<endl;
+                        cout<<"1.Tambah Menu"<<endl;
+                        cout<<"2.Cari dan  Ubah Menu"<<endl;
+                        cout<<"3.Hapus Menu"<<endl;
+                        cout<<"Log Out"<<endl;
+                        cin>>menuPengelola;
+                    }while (menuPengelola !=4);
+
+                    break;
+                case 2:
+                    cout<<"Silahkan Pilih Menu"<<endl;
+                    break;
+
+                case 3:
+                    cout<<"Aplikasi Dihentikan"<<endl;
+                    exit(1);
+
+                default:
+                    cout<<"Pilihan Tidak Tersedia"<<endl;
+                    break;
+            }
+
+           }while (pilihan !=4);
+           getch();
+
+         return 0;
+        }
